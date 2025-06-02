a020e86e7894443828f709af20554e0e9874fa79
njongefred <frednjonge9@gmail.com>
2025-06-02 19:55:26 +0300

    create an error handler

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import { db } from './libs/dbConnect.js'
 import userRouter from './routes/user.route.js'
+import { errorHandler } from './libs/middleware.js';
 
 const app = express();
 
@@ -15,6 +16,8 @@ app.use('/api/v1/users', userRouter);
 app.use((req, res) => {
   res.status(404).json({ message: 'not found' });
 });
+
+app.use(errorHandler)
 
 // Start the Express server
 app.listen(PORT, () => {
