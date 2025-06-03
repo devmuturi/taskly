a0e3772d44df322d51b6bb4390ebac9acdd0f2f6
njongefred <frednjonge9@gmail.com>
2025-06-04 00:48:48 +0300

    Add chakra ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/signin' element={ <SignIn/>} />
-        <Route path='/signup' element={ <SignUp/>} />
-      </Routes>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/signin' element={ <SignIn/>} />
+          <Route path='/signup' element={ <SignUp/>} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   )
 }
